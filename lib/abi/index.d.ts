c6b3d95a9d9593ec26ad3dbe7df3932610648295
lihang <lihang1329@gmail.com>
2022-08-16 16:34:13 +0000

    feat: 交易挖矿页面数据

---
+++ lib/abi/index.d.ts
@@ -0,0 +1,6 @@
+import { JsonFragment } from "@ethersproject/abi";
+declare const IERC20: JsonFragment[];
+declare const Multicall2: JsonFragment[];
+declare const Banana: JsonFragment[];
+declare const BananaClaimable: JsonFragment[];
+export { IERC20, Multicall2, Banana, BananaClaimable };
---
+++ lib/service/abi/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * 合约接口实现
+ */
+export * from './ERC20';
+export * from './MultiCallContract';
