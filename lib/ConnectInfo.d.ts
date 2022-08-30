f38b1b311adb7ddbb22955a68d0bc9cab75be8fe
lihang <lihang1329@gmail.com>
2022-08-30 07:56:27 +0000

    feat: update airdrop

---
+++ lib/ConnectInfo.d.ts
@@ -0,0 +1,66 @@
+import { AddressInfo, Erc20Service, MultiCallContract, RequestService, TransactionService } from './service';
+import { providers, Signer } from 'ethers';
+import { Provider } from '@ethersproject/providers';
+import { TradeMiningService } from './service/TradeMiningService';
+import { AirdropService } from "./service/AirdropService";
+export declare type Newable<T extends object> = new (...args: any[]) => T;
+export declare class ConnectInfo {
+    private _provider;
+    private _wallet;
+    private _status;
+    private _msg;
+    private _account;
+    private _chainId;
+    private _addressInfo;
+    private _instanceCache;
+    create<T extends object>(clazz: Newable<T>, ...args: any[]): T;
+    clear(): void;
+    /**
+     * 获取 ERC20 API
+     */
+    erc20(): Erc20Service;
+    /**
+     * 获取交易API
+     */
+    tx(): TransactionService;
+    tradeMining(): TradeMiningService;
+    airdrop(): AirdropService;
+    /**
+     * request service
+     */
+    api(): RequestService;
+    /**
+     * multiCall service
+     */
+    multiCall(): MultiCallContract;
+    get provider(): providers.JsonRpcProvider;
+    set provider(value: providers.JsonRpcProvider);
+    /**
+     * 获取连接的状态
+     */
+    get status(): boolean;
+    set status(value: boolean);
+    /**
+     * 获取连接的消息
+     */
+    get msg(): string;
+    set msg(value: string);
+    /**
+     * 获取连接的地址
+     */
+    get account(): string;
+    set account(value: string);
+    /**
+     * 获取连接的网络ID
+     */
+    get chainId(): number;
+    set chainId(value: number);
+    /**
+     * 获取连接的地址信息
+     */
+    get addressInfo(): AddressInfo;
+    set addressInfo(value: AddressInfo);
+    get wallet(): Signer;
+    set wallet(value: Signer);
+    getWalletOrProvider(): Signer | Provider;
+}
