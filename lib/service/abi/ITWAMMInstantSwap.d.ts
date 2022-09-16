34a9f2b7139758e160e8003c5fb2f5ffd6bb4270
lihang <lihang1329@gmail.com>
2022-09-16 07:22:04 +0000

    fix: redeem 精度的问题

---
+++ lib/service/abi/ITWAMMInstantSwap.d.ts
@@ -0,0 +1,11 @@
+import { BaseService } from '../BaseService';
+import { ConnectInfo } from '../../ConnectInfo';
+import { Contract } from 'ethers';
+import { MulContract } from '../../mulcall';
+import { TransactionEvent } from '../vo';
+export declare class ITWAMMInstantSwap extends BaseService {
+    TWAMMInstantSwapInstance: MulContract;
+    TWAMMInstantSwapContract: Contract;
+    constructor(connectInfo: ConnectInfo);
+    instantSwapTokenToToken(token0: string, token1: string, amountIn: string, amountOutMin: string, deadline: string): Promise<TransactionEvent>;
+}
