7ad3ce21a8a44892de2c4ad5efda9bcbbe142e28
lihang <lihang1329@gmail.com>
2022-09-16 09:17:51 +0000

    feat: claim走api获取

---
+++ lib/WalletConnect.d.ts
@@ -0,0 +1,24 @@
+import { ConnectInfo } from './ConnectInfo';
+import { providers, Wallet } from 'ethers';
+export declare class PrivateWallet {
+    provider: providers.JsonRpcProvider;
+    wallet: Wallet;
+}
+export declare type WalletType = PrivateWallet | providers.Web3Provider | {
+    provider: any;
+};
+export declare type ChainName = 'Arbitrum' | 'Arbitrum Testnet' | 'rinkeby' | 'mainnet' | 'goerli';
+export declare class WalletConnect {
+    wallet: WalletType;
+    connectInfo: ConnectInfo;
+    constructor(walletName: WalletType);
+    disConnect(): void;
+    update(): void;
+    privateWallet(): Promise<void>;
+    web3Provider(): Promise<void>;
+    /**
+     * 链接钱包
+     * @returns
+     */
+    connect(): Promise<ConnectInfo>;
+}
