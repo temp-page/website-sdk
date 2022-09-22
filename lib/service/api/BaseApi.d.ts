ca0d8296ccfaf8d23d9e0b42373362736ad1e8a1
lihang <lihang1329@gmail.com>
2022-09-22 05:22:00 +0000

    feat: 增加redeem开始时间的状态

---
+++ lib/service/api/BaseApi.d.ts
@@ -0,0 +1,16 @@
+import { Variables } from 'graphql-request/dist/types';
+import { ConnectInfo } from '../../ConnectInfo';
+import { AddressInfo } from '../vo';
+export declare const GRAPH_BANANA_PATH = "/banana";
+export declare class BaseApi {
+    readonly graphApi: string;
+    baseApi: BaseApi;
+    constructor(graphApi: string);
+    request<T = any>(path: string, method: 'get' | 'post' | 'put' | 'delete', data: any, config?: any): Promise<T>;
+    apiBaseRequest<T = any>(path: string, method: 'get' | 'post' | 'put' | 'delete', data: any, config?: any): Promise<T>;
+    graph<T = any, V = Variables>(path: string, query: string, variables: V, l2?: boolean): Promise<T>;
+    graphBase<T = any, V = Variables>(fullUrl: string, query: string, variables: V): Promise<T>;
+    bananaGraph<T = any, V = Variables>(query: string, variables: V): Promise<T>;
+    connectInfo(): ConnectInfo;
+    address(): AddressInfo;
+}
