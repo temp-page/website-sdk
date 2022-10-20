05ce5917b3296f34cc0f752e92d13d61b8eafa17
lihang <lihang1329@gmail.com>
2022-10-20 08:29:10 +0000

    feat: 地址初始化使用接口返回修改

---
+++ lib/service/abi/IBuybackPool.d.ts
@@ -0,0 +1,13 @@
+import { BaseService } from '../BaseService';
+import { ConnectInfo } from '../../ConnectInfo';
+import { ContractCall, MulContract } from '../../mulcall';
+import { Contract } from "ethers";
+export interface ShapeWithLabel {
+    [item: string]: ContractCall | string;
+}
+export declare class IBuybackPool extends BaseService {
+    buybackPoolInstance: MulContract;
+    buybackPoolContract: Contract;
+    constructor(connectInfo: ConnectInfo);
+    lastBuyingRate(): Promise<string>;
+}
