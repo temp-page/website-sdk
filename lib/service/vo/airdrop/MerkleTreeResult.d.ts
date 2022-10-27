4a27677d1cd312c817b9554b1ea7cabe70165580
lihang <lihang1329@gmail.com>
2022-10-27 03:29:37 +0000

    fix: 默认环境使用qa

---
+++ lib/service/vo/airdrop/MerkleTreeResult.d.ts
@@ -0,0 +1,33 @@
+/**
+ * MerkleTree 活动
+ */
+import { TransactionEvent } from '../TransactionEvent';
+export declare class MerkleTreeResult {
+    /**
+     * 是否领取 (优先级低)
+     */
+    isClaim: boolean;
+    /**
+     * 是否存在记录 (优先级高)
+     */
+    isExist: boolean;
+    /**
+     *  显示金额
+     */
+    showAmount: string;
+    /**
+     * USD Price*
+     */
+    usdPrice: string;
+    /**
+     * totalUsd*
+     */
+    totalUsd: string;
+    isJoined: boolean;
+    claim_end_time: number;
+    stat_end_time: number;
+    claim_start_time: number;
+    trader_pools: Array<any>;
+    kol_pools: Array<any>;
+    claim: () => Promise<TransactionEvent>;
+}
