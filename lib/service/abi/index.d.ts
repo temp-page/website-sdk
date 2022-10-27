196da9aef0500f00b51c3f33fe8456bc8bd42efa
lihang <lihang1329@gmail.com>
2022-10-27 10:08:03 +0000

    fix: unclaimedReward 精度处理

---
+++ lib/service/abi/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * 合约接口实现
+ */
+export * from './ERC20';
+export * from './IBanana';
+export * from './IBananaClaimable';
+export * from './IPair';
+export * from './ITWAMMInstantSwap';
+export * from './MerkleDistributor';
+export * from './MultiCallContract';
