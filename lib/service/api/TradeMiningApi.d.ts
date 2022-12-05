d9f933a46428c90d10dc355ec04a607d846f78df
lihang <lihang1329@gmail.com>
2022-12-05 02:52:04 +0000

    fix: 修复结束状态判断错误的问题

---
+++ lib/service/api/TradeMiningApi.d.ts
@@ -0,0 +1,33 @@
+import { BaseApi } from './BaseApi';
+import { TradeMiningActionsHistory, TradeMiningBBP, TradeMiningTotalGains } from '../vo';
+/**
+ * Bond API
+ */
+export declare class TradeMiningApi {
+    baseApi: BaseApi;
+    constructor(baseApi: BaseApi);
+    getBananaPrice(): Promise<{
+        price: string;
+    }>;
+    getBananaRate(): Promise<{
+        rate: string;
+        bananaPrice: string;
+    }>;
+    getBuyingRate(): Promise<string>;
+    getSwapInfo(): Promise<{
+        rate: string;
+        bananaPrice: string;
+        bananaBurns: string;
+        totalSpents: string;
+        boostUpRates: string;
+    }>;
+    getActionsHistory(user: string): Promise<{
+        totalGains: TradeMiningTotalGains;
+        history: TradeMiningActionsHistory[];
+    }>;
+    getBBP(user: string): Promise<TradeMiningBBP>;
+    getRedeemRate(): Promise<{
+        rate: string;
+        timestamp: string;
+    }[]>;
+}
---
+++ lib/service/vo/index.d.ts
@@ -0,0 +1,7 @@
+export * from './AddressInfo';
+export * from './Types';
+export * from './TransactionEvent';
+export * from './tradeMining';
+export * from './Kline';
+export * from './airdrop';
+export * from './bonus';
