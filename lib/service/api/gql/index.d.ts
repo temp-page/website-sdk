afd1e6eb886e9ac9e2092fc6e4effb3c8f776d07
lihang <lihang1329@gmail.com>
2022-12-07 12:05:17 +0000

    fix: web3钱包对接

---
+++ lib/service/api/gql/index.d.ts
@@ -0,0 +1,5 @@
+export declare function getBananaApexRateLogs(): string;
+export declare function getSwapInfo(): string;
+export declare function getActionsHistory(): string;
+export declare function getBBPData(): string;
+export declare function getRedeemRate(): string;
