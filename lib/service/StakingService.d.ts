3cd597d970effab4fd7bd24680e82e05de25ee66
lihang <lihang1329@gmail.com>
2022-12-23 10:26:51 +0000

    feat: bybit wallet gas Price调整成中档

---
+++ lib/service/StakingService.d.ts
@@ -0,0 +1,7 @@
+import { ConnectInfo } from "../ConnectInfo";
+import { BaseService } from "./BaseService";
+import { StakingInfo } from "./vo";
+export declare class StakingService extends BaseService {
+    constructor(connectInfo: ConnectInfo);
+    stakingInfo(): Promise<StakingInfo>;
+}
