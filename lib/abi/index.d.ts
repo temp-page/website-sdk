f7288a07d56cbe19f423a8a360ebd92f92ed3850
lihang941 <1329075357@qq.com>
2023-01-09 03:57:16 +0000

    feat: 接口调用联调

---
+++ lib/abi/index.d.ts
@@ -0,0 +1,13 @@
+import { JsonFragment } from '@ethersproject/abi';
+declare const IERC20: JsonFragment[];
+declare const Multicall2: JsonFragment[];
+declare const Banana: JsonFragment[];
+declare const BananaClaimable: JsonFragment[];
+declare const Pair: JsonFragment[];
+declare const TWAMMInstantSwap: JsonFragment[];
+declare const IMerkleDistributor: JsonFragment[];
+declare const BuybackPool: JsonFragment[];
+declare const IEsAPEX2: JsonFragment[];
+declare const StakingPool2: JsonFragment[];
+declare const ApeXPool2: JsonFragment[];
+export { IERC20, Multicall2, Banana, BananaClaimable, Pair, TWAMMInstantSwap, IMerkleDistributor, BuybackPool, IEsAPEX2, StakingPool2, ApeXPool2, };
