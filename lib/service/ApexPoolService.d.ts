925173a3b34306f0e1a547cc11b5f46912f7f2c2
lihang941 <1329075357@qq.com>
2023-01-10 09:53:48 +0000

    feat: 增加round字段

---
+++ lib/service/ApexPoolService.d.ts
@@ -0,0 +1,10 @@
+import { BaseService } from './BaseService';
+import { ConnectInfo } from '../ConnectInfo';
+import { ApexPoolStakeInfo } from './vo';
+/**
+ * Bonus API
+ */
+export declare class ApexPoolService extends BaseService {
+    constructor(connectInfo: ConnectInfo);
+    stakeInfo(privateApi: any): Promise<ApexPoolStakeInfo>;
+}
