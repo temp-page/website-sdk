b12fc647d730fb1d6d849a64831ab6332fa708e2
lihang941 <1329075357@qq.com>
2023-01-13 02:42:07 +0000

    feat: 修复获取参数的问题

---
+++ lib/service/abi/IBanana.d.ts
@@ -0,0 +1,11 @@
+import { BaseService } from '../BaseService';
+import { ConnectInfo } from '../../ConnectInfo';
+import { Contract } from 'ethers';
+import { MulContract } from '../../mulcall';
+import { TransactionEvent } from '../vo';
+export declare class IBanana extends BaseService {
+    bananaInstance: MulContract;
+    bananaContract: Contract;
+    constructor(connectInfo: ConnectInfo);
+    redeem(amount: string): Promise<TransactionEvent>;
+}
