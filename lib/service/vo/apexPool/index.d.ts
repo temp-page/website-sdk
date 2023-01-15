ea45acf0eadaf520c39b6c20a6cfc456accc66d7
lihang941 <1329075357@qq.com>
2023-01-15 07:46:04 +0000

    feat: 用户APY计算调整

---
+++ lib/service/vo/apexPool/index.d.ts
@@ -0,0 +1,87 @@
+import { TransactionEvent } from '../TransactionEvent';
+export declare class ApexPoolStakeInfo {
+    /**
+     * APY %
+     */
+    apy: string;
+    /**
+     * USER APY %
+     */
+    userApy: string;
+    /**
+     * Total Stake Apex
+     */
+    totalStakeApex: string;
+    /**
+     * Total Stake EsApex
+     */
+    totalStakeEsApex: string;
+    /**
+     * Reward Last Period(USDC)
+     */
+    rewardLastPeriod: string;
+    /**
+     * Unclaim Rewards(USDC)
+     */
+    unclaimRewards: string;
+    /**
+     * Claim Rewards(USDC)
+     */
+    claimRewards: string;
+    /**
+     * Total Earned Reward(USDC)
+     */
+    totalEarnedReward: string;
+    /**
+     * User Staked APEX
+     */
+    userStakedAPEX: string;
+    /**
+     * User Staked esAPEX
+     */
+    userStakedEsAPEX: string;
+    /**
+     * 是否需要授权Apex
+     */
+    showApexApprove: boolean;
+    /**
+     * 是否需要授权EsApex
+     */
+    showEsApexApprove: boolean;
+    /**
+     * APEX的余额
+     */
+    apexBalance: string;
+    /**
+     * EsApex的余额
+     */
+    esApexBalance: string;
+    round: number;
+    rewardList: {
+        rewardId: string;
+        amount: string;
+        token: string;
+        time: string | number;
+        canClaim: boolean;
+        appId: string;
+    }[];
+    stakingHoldingsList: {
+        round: number;
+        stakeId: string;
+        stakeToken: string;
+        stakeAmount: string;
+        totalFactor: string;
+        t2EFactor: string;
+        timeFactor: string;
+        stakeDuration: string;
+        updateTime: number;
+    }[];
+    approve: (token: 'apex' | 'esApex') => Promise<TransactionEvent>;
+    stake: (token: 'apex' | 'esApex', amount: string) => Promise<TransactionEvent>;
+    preStake: (token: 'apex' | 'esApex') => Promise<{
+        t2eFactor: string;
+        timeFactor: string;
+        totalFactor: string;
+    }>;
+    unStake: (token: 'apex' | 'esApex', amount: string) => Promise<TransactionEvent>;
+}
