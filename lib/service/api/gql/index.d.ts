c746e383bab5eafe0e716f716169548b053dfec0
lihang941 <1329075357@qq.com>
2023-03-02 13:31:46 +0000

    feat: 增加RPC缓存,在未链接钱包的场景下减少RPC的调用

---
+++ lib/service/api/gql/index.d.ts
@@ -0,0 +1,9 @@
+export declare function getBananaApexRateLogs(): string;
+export declare function getSwapInfo(): string;
+export declare function getActionsHistory(): string;
+export declare function getUserActionsHistory(): string;
+export declare function getBBPData(): string;
+export declare function getRedeemRate(): string;
+export declare function getStakingInfo(): string;
+export declare function getApexPoolStakeInfos(): string;
+export declare function getUserApexPoolStakeInfos(): string;
