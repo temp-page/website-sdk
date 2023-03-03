576934b3c0b042291d6cdfc014d057a1ca7335b9
lihang941 <1329075357@qq.com>
2023-03-03 09:03:57 +0000

    fix: JsonRpcProvider 替换成 StaticJsonRpcProvider,放在每次call调用chainId

---
+++ lib/service/tool/ApexPoolMathTool.d.ts
@@ -0,0 +1,26 @@
+interface PoolStakeInfo {
+    avgStakeTime: string;
+    lastUserStakeAmount: string;
+}
+export declare class ApexPoolMathTool {
+    /**
+     * 计算APY
+     * @param totalRewardsLastWeek 上周总奖励 USD
+     * @param totalStakeLastWeek 上周质押 apex + esAPex
+     * @param apexPrice APEX 价格 USD
+     * @param stakeDay 质押天数
+     */
+    static APY(totalRewardsLastWeek: string, totalStakeLastWeek: string, apexPrice: string, stakeDay?: string): {
+        apy: string;
+    };
+    /**
+     * 计算 totalFactor
+     */
+    static totalFactor(t2eFactor: string, timeFactor: string): {
+        totalFactor: string;
+    };
+    static avgStakeTime(apexStakeInfo: PoolStakeInfo, esApexStakeInfo: PoolStakeInfo): {
+        avgStakeTime: string;
+    };
+}
+export {};
