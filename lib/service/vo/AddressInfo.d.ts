db15a187e3086c3ddd0b5c1b9ae171642397947c
lihang941 <1329075357@qq.com>
2023-07-03 08:54:06 +0000

    feat: ParticleNetwork

---
+++ lib/service/vo/AddressInfo.d.ts
@@ -0,0 +1,43 @@
+import { ConnectInfo } from "../../ConnectInfo";
+import { RequestService } from "../RequestService";
+/**
+ * 地址信息
+ */
+export declare class AddressInfo {
+    apexProBaseUrl: string;
+    bananaGraphApi: string;
+    /**
+     * chainID
+     */
+    chainId: number;
+    /**
+     * 链上区块浏览器地址
+     */
+    scan: string;
+    rpc: string;
+    multicall: string;
+    banana: string;
+    bananaClaimable: string;
+    apex: string;
+    esApex: string;
+    apexPool: string;
+    usdc: string;
+    pair: string;
+    twammRouter: string;
+    buybackPool: string;
+    stakingPool: string;
+    arbiChainId: number;
+    arbiApex: string;
+    arbiApexPool: string;
+    arbiMulticall: string;
+    arbiBananaGraphApi: string;
+    arbiScan: string;
+    arbiAddressList: string[];
+    readonlyConnectInfoInstance: ConnectInfo;
+    apiService: RequestService;
+    readonlyConnectInfo(): ConnectInfo;
+    getApiService(): RequestService;
+    getEtherscanAddress(address: any): string;
+    getEtherscanTx(tx: any): string;
+    getArbiScanTx(tx: any): string;
+}
