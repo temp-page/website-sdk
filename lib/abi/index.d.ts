9c0b2a77b8e893e839a270436c1fa42513c367e7
lihang941 <1329075357@qq.com>
2024-01-21 15:41:45 +0000

    feat: t2e-usdt

---
+++ lib/abi/index.d.ts
@@ -0,0 +1,13 @@
+import { JsonFragment } from '@ethersproject/abi';
+export declare const IERC20: JsonFragment[];
+export declare const Multicall2: JsonFragment[];
+export declare const Banana: JsonFragment[];
+export declare const BananaClaimable: JsonFragment[];
+export declare const Pair: JsonFragment[];
+export declare const TWAMMInstantSwap: JsonFragment[];
+export declare const IMerkleDistributor: JsonFragment[];
+export declare const BuybackPool: JsonFragment[];
+export declare const IEsAPEX2: JsonFragment[];
+export declare const StakingPool2: JsonFragment[];
+export declare const ApeXPool2: JsonFragment[];
+export declare const BananaUsdtPair: JsonFragment[];
---
+++ lib/service/api/index.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Graph node 接口实现
+ */
+export * from './BaseApi';
+export * from './gql';
+export * from './TwammTool';
+export * from './TradeMiningApi';
+export * from './TradeMiningV2Api';
+export * from './Web3WalletApi';
