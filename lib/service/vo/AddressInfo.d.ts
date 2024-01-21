696e4ae3d3c5d81c09eba7fec9be5e91945b23b8
lihang941 <1329075357@qq.com>
2024-01-21 06:53:55 +0000

    Merge branch 'feat/dragon' into test

---
+++ lib/service/vo/AddressInfo.d.ts
@@ -0,0 +1,60 @@
+import { ConnectInfo } from "../../ConnectInfo";
+import { RequestService } from "../RequestService";
+export interface DragonBallAddressConfig {
+    DragonBallConfig: string;
+    DivineDragoNft: string;
+    DragonBall: string;
+    DragonBallNfts: string[];
+}
+/**
+ * 地址信息
+ */
+export declare class AddressInfo {
+    apexProBaseUrl: string;
+    bananaGraphApi: string;
+    bananaV2GraphApi: string;
+    /**
+     * chainID
+     */
+    chainId: number;
+    /**
+     * 链上区块浏览器地址
+     */
+    scan: string;
+    rpc: string;
+    multicall: string;
+    banana: string;
+    bananaClaimable: string;
+    apex: string;
+    esApex: string;
+    apexPool: string;
+    usdc: string;
+    usdt: string;
+    bananaV2: string;
+    apexV2: string;
+    bananaClaimableV2: string;
+    bananaBurnFrom: string;
+    bananaUsdtPair: string;
+    pair: string;
+    twammRouter: string;
+    buybackPool: string;
+    stakingPool: string;
+    arbRpc: string;
+    arbiChainId: number;
+    arbiApex: string;
+    arbiApexPool: string;
+    arbiMulticall: string;
+    arbiBananaGraphApi: string;
+    arbiScan: string;
+    arbiAddressList: string[];
+    apiService: RequestService;
+    dragonBall: DragonBallAddressConfig;
+    readonlyConnectInfoInstance: ConnectInfo;
+    readonlyConnectInfo(): ConnectInfo;
+    arbiReadonlyConnectInfoInstance: ConnectInfo;
+    arbiReadonlyConnectInfo(): ConnectInfo;
+    getApiService(): RequestService;
+    getEtherscanAddress(address: string): string;
+    getEtherscanTx(tx: string): string;
+    getArbiScanTx(tx: string): string;
+}
