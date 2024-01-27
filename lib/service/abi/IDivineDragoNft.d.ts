dbc0a6e596984356ac4c10194c22dee5efc1c2fa
lihang941 <1329075357@qq.com>
2024-01-27 05:06:39 +0000

    feat: 榜单

---
+++ lib/service/abi/IDivineDragoNft.d.ts
@@ -0,0 +1,5 @@
+import { ConnectInfo } from "../../ConnectInfo";
+import { BaseAbi } from "./BaseAbi";
+export declare class IDivineDragoNft extends BaseAbi {
+    constructor(connectInfo: ConnectInfo);
+}
