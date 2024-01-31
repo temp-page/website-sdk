2674212f59d59d797cc05af99f895e41cb1587e5
lihang941 <1329075357@qq.com>
2024-01-31 06:58:17 +0000

    feat: 榜单

---
+++ lib/service/api/BaseApi.d.ts
@@ -0,0 +1,16 @@
+import { Variables } from 'graphql-request/dist/types';
+import { ConnectInfo } from '../../ConnectInfo';
+import { AddressInfo } from '../vo';
+export declare class BaseApi {
+    request<T = any>(path: string, method: 'get' | 'post' | 'put' | 'delete', data: any, config?: any): Promise<T>;
+    apiBaseRequest<T = any>(path: string, method: 'get' | 'post' | 'put' | 'delete', data: any, config?: any): Promise<T>;
+    graphBase<T = any, V = Variables>(fullUrl: string, query: string, variables: V): Promise<T>;
+    bananaGraph<T = any, V = Variables>(query: string, variables: V): Promise<T>;
+    bananaV2Graph<T = any, V = Variables>(query: string, variables: V): Promise<T>;
+    arbiBananaGraph<T = any, V = Variables>(query: string, variables: V): Promise<T>;
+    arbiDragonNftGraph<T = any, V = Variables>(query: string, variables: V): Promise<T>;
+    connectInfo(): ConnectInfo;
+    arbiConnectInfo(): ConnectInfo;
+    address(): AddressInfo;
+}
+export declare const BASE_API: BaseApi;
