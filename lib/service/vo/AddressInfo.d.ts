93cba24cc7243a811306f051158ae1df0adb25ca
lihang941 <1329075357@qq.com>
2024-02-27 08:55:04 +0000

    fix: 定期时间处理问题

---
+++ lib/service/vo/AddressInfo.d.ts
@@ -0,0 +1,81 @@
+import { ConnectInfo } from "../../ConnectInfo";
+import { RequestService } from "../RequestService";
+export interface DragonBallAddressConfig {
+    DragonBallConfig: string;
+    DivineDragoNft: string;
+    DragonBall: string;
+    DragonBallNfts: string[];
+}
+export interface ApexRegularStakingConfig {
+    common: {
+        "apex": string;
+        "esApex": string;
+        "apexPoolV2": string;
+        "apexPoolV3": string;
+        "graphUrlV2": string;
+        "graphUrlV3": string;
+    };
+    arbi: {
+        "apex": string;
+        "esApex": string;
+        "apexPoolV2": string;
+        "apexPoolV3": string;
+        "graphUrlV2": string;
+        "graphUrlV3": string;
+    };
+}
+/**
+ * 地址信息
+ */
+export declare class AddressInfo {
+    env: string;
+    apexProBaseUrl: string;
+    bananaGraphApi: string;
+    bananaV2GraphApi: string;
+    /**
+     * chainID
+     */
+    chainId: number;
+    /**
+     * 链上区块浏览器地址
+     */
+    scan: string;
+    rpc: string;
+    multicall: string;
+    banana: string;
+    bananaClaimable: string;
+    apex: string;
+    esApex: string;
+    apexPool: string;
+    usdc: string;
+    usdt: string;
+    bananaV2: string;
+    apexV2: string;
+    bananaClaimableV2: string;
+    bananaBurnFrom: string;
+    bananaUsdtPair: string;
+    pair: string;
+    twammRouter: string;
+    buybackPool: string;
+    stakingPool: string;
+    arbRpc: string;
+    arbiChainId: number;
+    arbiApex: string;
+    arbiApexPool: string;
+    arbiMulticall: string;
+    arbiBananaGraphApi: string;
+    arbiDragonNftGraphApi: string;
+    arbiScan: string;
+    arbiAddressList: string[];
+    apiService: RequestService;
+    dragonBall: DragonBallAddressConfig;
+    apexRegularStaking: ApexRegularStakingConfig;
+    readonlyConnectInfoInstance: ConnectInfo;
+    readonlyConnectInfo(): ConnectInfo;
+    arbiReadonlyConnectInfoInstance: ConnectInfo;
+    arbiReadonlyConnectInfo(): ConnectInfo;
+    getApiService(): RequestService;
+    getEtherscanAddress(address: string): string;
+    getEtherscanTx(tx: string): string;
+    getArbiScanTx(tx: string): string;
+}
