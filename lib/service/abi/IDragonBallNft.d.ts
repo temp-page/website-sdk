e8952a5fea2a73014fd53d9aa225ec9c180d3233
lihang941 <1329075357@qq.com>
2024-02-28 08:29:31 +0000

    feat: Allowance 问题

---
+++ lib/service/abi/IDragonBallNft.d.ts
@@ -0,0 +1,5 @@
+import { BaseAbi } from "./BaseAbi";
+import { ConnectInfo } from "../../ConnectInfo";
+export declare class IDragonBallNft extends BaseAbi {
+    constructor(connectInfo: ConnectInfo, address: string);
+}
