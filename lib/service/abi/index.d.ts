4f3de2f807258629ef149a74bd817cd24b000160
lihang941 <1329075357@qq.com>
2024-03-05 03:15:31 +0000

    fix: status

---
+++ lib/service/abi/index.d.ts
@@ -0,0 +1,22 @@
+/**
+ * 合约接口实现
+ */
+export * from './ERC20';
+export * from './IBanana';
+export * from './IBananaV2';
+export * from './IBananaClaimable';
+export * from './IBananaClaimableV2';
+export * from './IPair';
+export * from './ITWAMMInstantSwap';
+export * from './MerkleDistributor';
+export * from './MultiCallContract';
+export * from './IStakingPool2';
+export * from './IApexPool2';
+export * from './IApexPool2V2';
+export * from './IApexPool3';
+export * from './IBananaUsdtPair';
+export * from './IDivineDragoNft';
+export * from './IDragonBall';
+export * from './IDragonBallConfig';
+export * from './IDragonBallNft';
+export * from './BaseAbi';
