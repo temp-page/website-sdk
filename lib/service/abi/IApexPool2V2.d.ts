8772b76c0a2bcdc7f02fdd0711a091a4c32b87b8
lihang941 <1329075357@qq.com>
2024-03-06 10:14:16 +0000

    fix: readonlyConnectInfo

---
+++ lib/service/abi/IApexPool2V2.d.ts
@@ -0,0 +1,14 @@
+import { BaseService } from '../BaseService';
+import { ConnectInfo } from '../../ConnectInfo';
+import { Contract } from 'ethers';
+import { MulContract } from '../../mulcall';
+import { TransactionEvent } from '../vo';
+export declare class IApexPool2V2 extends BaseService {
+    apexPoolInstance: MulContract;
+    apexPoolContract: Contract;
+    constructor(connectInfo: ConnectInfo);
+    stakeAPEX(accountId: string | number, amount: string): Promise<TransactionEvent>;
+    stakeEsAPEX(accountId: string | number, amount: string): Promise<TransactionEvent>;
+    unstakeAPEX(to: string, accountId: string | number, amount: string): Promise<TransactionEvent>;
+    unstakeEsAPEX(to: string, accountId: string | number, amount: string): Promise<TransactionEvent>;
+}
