ddb2dae0394897019e52aec1282d130069a012ef
lihang941 <1329075357@qq.com>
2024-03-06 08:53:32 +0000

    fix: unstakeEsAPEX

---
+++ lib/service/tool/MathTool.d.ts
@@ -0,0 +1,6 @@
+export declare class MathTool {
+    static getAmountOut(amountIn: string, reserveIn: string, reserveOut: string): string;
+    static impact(amountIn: string, amountOut: string, reserveIn: string, reserveOut: string): string;
+    static deadline(deadline: string): string;
+    static quote(amountA: string, reserveA: string, reserveB: string): string;
+}
